1171704e82ee784a59e0ff0e50310a77fc63efa6
robertoamato <robertoamatoz@gmail.com>
2024-10-07 15:06:41 -0300

    diretiva estrutural ng if

---
+++ 15+-+Diretiva+Estrutural+NgIf/src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  minhaProp = true;
+  pessoa = {
+    name: undefined,
+    status: 1,
+  };
+
+  toggleProp() {
+    this.minhaProp = !this.minhaProp;
+  }
+
+  isTruthy() {
+    // return {};
+    // return [];
+    // return true;
+    // return 1;
+    // return "teste";
+    // return "0";
+    // return "false";
+    // return this.pessoa.status;
+  }
+
+  isFalsy() {
+    // return false;
+    // return 0;
+    // return "";
+    // return '';
+    // return null;
+    // return undefined;
+    // return -0;
+    // return NaN;
+    // return this.pessoa.name;
+    // return this.pessoa.endereco?.rua;
+  }
+}
